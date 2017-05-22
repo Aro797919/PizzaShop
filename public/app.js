e0e0bf38407c9ed3b164fcd7d63b56b545eb7cb6
Araik7979 <aro797919@gamil.com>
2017-05-22 14:23:18 +0300

    update app.js

--- public/app.js
+++ public/app.js
@@ -7,7 +7,26 @@
     alert(x);
 }
 
- function add_to_cart()
-	{
-	  alert('Hello from function');
+function add_to_cart(id)
+{
+  var key = 'product_' + id;
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
+}
+  
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+  for(var i =0; i < window.localStorage.length; i++)
+{
+  var key = window.localStorage.key(i);// получаем ключ
+  var value = window.localStorage.getItem(key); //получаем значение,аналог в ruby hh[key] = x
+  if(key.indexOf('product_') == 0)
+   {
+   cnt = cnt + value * 1;
+   }
+}
+ return cnt;
 }
